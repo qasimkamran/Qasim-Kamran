f28f64f2b104f82b860c663d8194131720236ec5
qasimk <qasimworkaus@gmail.com>
2026-07-04 16:29:33 +0000

    Added the projects route

---
+++ website/src/app/projects/page.tsx
@@ -0,0 +1,8 @@
+export default function Index() {
+    return (
+        <main className="w-full max-w-3xl px-6 pb-12 pt-[20vh]">
+            <p className="italic">To be implemented.</p>
+        </main>
+    );
+}
+
